44283def8dc5ff3a98138b45e0d4e7606cf4ed4a
1000338 <a1000338@1000338ui-MacBookPro.local>
2023-10-20 20:51:15 +0900

    Initial commit

---
+++ src/main/java/mlmall/mlmalladminbe/MlmallAdminBeApplication.java
@@ -0,0 +1,13 @@
+package mlmall.mlmalladminbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlmallAdminBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlmallAdminBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mlmall/mlmalladminbe/MlmallAdminBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mlmall.mlmalladminbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlmallAdminBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
